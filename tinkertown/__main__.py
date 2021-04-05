9064dfda1e5ffc917ae575b23f9afd85f17ada83
Chris Wesseling <chris.wesseling@xs4all.nl>
2021-04-05 15:16:47 +0200

    Adds watchdog minotoring

--- tinkertown/__main__.py
+++ tinkertown/__main__.py
@@ -1,22 +1,58 @@
 from pathlib import Path
 
 import typer
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
-from .technician import (download_cards, download_portraits, in_progress,
-                         readcache,)
+from .technician import download_cards, download_portraits, in_progress
 
 app = typer.Typer()
 
 
+class EventHandler(FileSystemEventHandler):
+    def __init__(self, taskmap):
+        super().__init__()
+        self.taskmap = taskmap
+
+    def on_modified(self, event):
+        self.taskmap.get(event.src_path, lambda: None)()
+
+
 @app.command()
 def main(wineprefix: str) -> None:
-    path = next(Path(wineprefix).glob('**/HearthstoneDeckTracker/Images'))
+    try:
+        path = next(Path(wineprefix).glob('**/HearthstoneDeckTracker/Images'))
+    except StopIteration:
+        typer.echo(f'No decktracker found under {wineprefix}', err=True)
+        raise typer.Exit(1)
 
-    card_path = path / 'CardImages'
-    download_cards(readcache(card_path / 'Cache.xml'), card_path)
+    tasks = [
+        ('CardImages', download_cards),
+        ('CardPortraits', download_portraits),
+    ]
 
-    port_path = path / 'CardPortraits'
-    download_portraits(in_progress(port_path), port_path)
+    def make_task(d, f):
+        def task():
+            f(in_progress(path / d), path / d)
+        return task
+
+    handler = EventHandler(taskmap={
+        str(path / d / '_inProgress'): make_task(d, f)
+        for d, f in tasks
+    })
+
+    for p, f in handler.taskmap.items():
+        f()
+
+    observer = Observer()
+    observer.schedule(handler, path, recursive=True)
+    typer.echo(f'Monitoring {path} for changes. Stop with CTRL-C.')
+    observer.start()
+    try:
+        observer.join()
+    except KeyboardInterrupt:
+        observer.stop()
+        raise typer.Abort()
 
 
 if __name__ == '__main__':
